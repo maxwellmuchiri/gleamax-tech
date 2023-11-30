7b033c510f65cc985f7e4934da22075a7b1a8504
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-11-30 03:05:11 +0300

    css kidogo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Landing from './components/Landing';
 import SignList from './components/SignList';
-
+import './App.css';
 function App() {
   const signs = ['Metal Signs', 'Wooden Signs', 'Neon Signs', 'Custom Signs'];
 
